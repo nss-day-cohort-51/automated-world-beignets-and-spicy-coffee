ab2904ae71aad3ef5764c3ad5a17ae8d46b3b9e0
Ben Mosley <bmosley1132@gmail.com>
2021-08-24 16:58:49 -0400

    structural work

--- scripts/netherlands/celebrities/celebritiesDataProvider.js
+++ scripts/netherlands/celebrities/celebritiesDataProvider.js
@@ -1,5 +1,10 @@
+export const getCelebrities = () => {
+    return netherlandsCelebrities
+}
+
 const netherlandsCelebrities = [
     {
+        image: "./images/van-gogh.jpeg",
         name: "Vincent Van Gogh",
         occupation: "Artist",
         description: "Post-impressionist painter who created more than 2,100 artworks.",
@@ -7,6 +12,7 @@ const netherlandsCelebrities = [
     },
 
     {
+        image: "./images/van-halen.jpeg",
         name: "Eddie Van Halen",
         occupation: "Rockstar",
         description: "Lead guitarist and co-founder of the rock band Van Halen.",
@@ -14,15 +20,19 @@ const netherlandsCelebrities = [
     },
 
     {
+        image: "./images/gegard-mousasi-bellator-206-ceremonial-weigh-ins.jpeg",
         name: "Gegard Mousasi",
         occupation: "Mixed Martial Artist",
         description: "Current two-time Bellator Middleweight Champion and fourth in Bellator men's pound-for-pound rankings.",
         link: "https://en.wikipedia.org/wiki/Gegard_Mousasi"
+        
     },
     {
+        image: "./images/tiesto.jpeg",
         name: "Tiesto",
         occupation: "Electronic music producer and DJ",
         description: "Grammy Award Winning DJ/producer, regarded as the 'Godfather of EDM'",
         link: "https://www.tiesto.com/"
+        
     }
 ]
---
+++ scripts/netherlands/netherlands.js
@@ -0,0 +1,7 @@
+import { getCelebrities } from "./celebrities/celebritiesDataProvider.js";
+import { getLandmarks } from "./landmarks/landmarkDataProvider.js";
+import { getCities } from "./cities/cityDataProvider.js";
+
+getCelebrities();
+getLandmarks();
+getCities();
