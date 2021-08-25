2e7c1f8a8b77f87be7fced18cd548e4445ea04d3
Chris Murphy <theworldisyours@gmail.com>
2021-08-25 11:08:29 -0400

    first upload

---
+++ scripts/fiji/celebrities/celebritiesDataProvider.js
@@ -0,0 +1,32 @@
+const fijiCelebrities = [
+    {
+        name: "Jimmy 'Superfly' Snuka",
+        occupation: "Professional Wrestler",
+        description: "Professional career spanned from 1968 to mid-2015.",
+        link: "https://www.https://en.wikipedia.org/wiki/Jimmy_Snuka.com/"
+    },
+
+    {
+        name: "Lote Tuqiri",
+        occupation: "Professional Rugby Player",
+        description: "Professional career spanned from 1999 to 2014.",
+        link: "//https://en.wikipedia.org/wiki/Lote_Tuqiri"
+    },
+
+    {
+        name: "Ellia Green",
+        occupation: "Professional Rugby Player",
+        description: "2016 Olympic Gold medal winner in Rio de Janeiro for team Australia.",
+        link: "https://en.wikipedia.org/wiki/Ellia_Green"
+    },
+    {
+        name: "Mika Vukona",
+        occupation: "Professional Basketball Player",
+        description: "Professional career began in 2000 and is still going strong today.",
+        link: "https://en.wikipedia.org/wiki/Mika_Vukona"
+    }
+]
+
+export const getCelebrities = () => {
+    return fijiCelebrities
+}
