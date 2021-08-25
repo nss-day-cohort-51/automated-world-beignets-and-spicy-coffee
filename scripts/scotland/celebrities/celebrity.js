43a31b8450020c482300afc5e6b66fded9e115d2
joshuastewart417 <joshua.stewart780@gmail.com>
2021-08-25 11:38:50 -0500

    updated index.html with country images and updated scotland.html and js modules to populate page

---
+++ scripts/scotland/celebrities/celebrity.js
@@ -0,0 +1,11 @@
+export const celebrityCard = (celebrityObj) => {
+  return `<article class="celebrity-card">
+                <h4>${celebrityObj.name}</h4>
+                <ul>
+                    <img class="celebrity-image" src="images/${celebrityObj.image}" alt="celebrity image">
+                    <li class="celebrity-details">${celebrityObj.occupation}</li>
+                    <li class="celebrity-details">${celebrityObj.description}</li>
+                </ul>
+                <a href="${celebrityObj.link}" target="_blank">More Info</a>
+            </article>`;
+};
