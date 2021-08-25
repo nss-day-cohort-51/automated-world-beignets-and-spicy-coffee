e4efdc63a07055c5fbbbb0d1658cebdcbf883473
Tapolcsanyi <iceike237@gmail.com>
2021-08-25 11:08:19 -0500

    finished adding norway data

--- scripts/norway/landmarks/landmarkDataProvider.js
+++ scripts/norway/landmarks/landmarkDataProvider.js
@@ -1,21 +1,24 @@
 const landmarkCollection = [
     {
+        image: "images/landmark-vigeland.jpg",
         name: "Vigelandsanlegget",
         year: 1924,
         link: "https://vigeland.museum.no/en"
     },
     {
+        image: "images/landmark-akershus.jpg",
         name: "Akershus Fortress",
         year: 1299,
         link: "https://www.visitoslo.com/en/product/?tlp=2978773"
     },
     {
+        image: "images/landmark-viking.jpg",
         name: "Viking Ships Museum",
         year: 1957,
         link: "https://www.khm.uio.no/english/visit-us/viking-ship-museum/"
     }
 ]
 
-export const landmarkList = () => {
+export const getLandmark = () => {
     return landmarkCollection
 }
