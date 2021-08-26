adf6b485eb68042408f3f4985b87909592ac468a
Ben Mosley <bmosley1132@gmail.com>
2021-08-26 14:46:38 -0400

    final for netherlands

--- scripts/netherlands/landmarks/landmarks.js
+++ scripts/netherlands/landmarks/landmarks.js
@@ -3,10 +3,9 @@ export const landmarks = (landmarksObj) => {
 				<div><img class="landmarks-image" src="${landmarksObj.image}" /></div>
 				<h3 class="landmarks-name">${landmarksObj.name}</h3>
 				<ul>
-					<li class="landmarks-details"> Occupation: ${landmarksObj.occupation}</li>
-					<li class="landmarks-details">Description: ${landmarksObj.description} </li>
-					<li class="landmarks-details">Find Out More: ${landmarksObj.link}</li>
-					
+					<li class="landmarks-details"> Established: ${landmarksObj.year}</li>
+					<li class="landmarks-details"> ${landmarksObj.description} </li>
+					<li><a href="${landmarksObj.link}" target="_blank"> Find out more </a></li>
 				</ul>
         	</article>`
 }
