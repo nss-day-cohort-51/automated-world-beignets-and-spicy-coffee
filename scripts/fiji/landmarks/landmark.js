ce2d50d76588c67466ec225f15ad5f5d5f784703
Chris Murphy <theworldisyours@gmail.com>
2021-08-26 15:02:06 -0400

    one more try for the final

--- scripts/fiji/landmarks/landmark.js
+++ scripts/fiji/landmarks/landmark.js
@@ -1,9 +1,9 @@
 export const landmarkCard = (landmarkObj) => {
     return `<article class="landmark-card">
                   <h4>${landmarkObj.name}</h4>
-                  <ul>
-                      <img class="landmark-image" src="images/${landmarkObj.image}" alt="landmark image">
-                      <li class="landmark-details">${landmarkObj.dateCreated}</li>
+                  <img class="landmark-image" src="images/${landmarkObj.image}" alt="landmark image">
+                  <ul>                      
+                      <li class="landmark-details">${landmarkObj.year}</li>
                       <li class="landmark-details">${landmarkObj.description}</li>
                   </ul>
                   <a href="${landmarkObj.link}" target="_blank">More Info</a>
