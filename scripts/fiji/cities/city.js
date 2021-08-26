a4396dfda46a2e39cde9595455e523385a374de3
Chris Murphy <theworldisyours@gmail.com>
2021-08-26 11:11:50 -0400

    added images for celeb/city/landmark

---
+++ scripts/fiji/cities/city.js
@@ -0,0 +1,11 @@
+export const cityCard = (cityObj) => {
+    return `<article class="city-card">
+                  <h4>${cityObj.name}</h4>
+                  <ul>
+                      <img class="city-image" src="images/${cityObj.image}" alt="city image">
+                      <li class="city-details">Population: ${cityObj.population}</li>
+                      <li class="city-details">${cityObj.description}</li>
+                  </ul>
+                  <a href="${cityObj.link}" target="_blank">More Info</a>
+              </article>`;
+  };
